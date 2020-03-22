7d27391a590c7076726f9bb35e224b0ce68ec584
ChesyaWhite <vyacheslavbelyai@gmail.com>
2020-03-22 13:02:55 +0300

    init: start "morse-decoder" task

--- src/index.js
+++ src/index.js
@@ -35,12 +35,26 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****':  ' ',
 };
-
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    arr = [];
+    arr2 = [];
+    str = '';
+    for (let i = 0; i < (expr.length/2); i++) {
+            arr[i] = expr.slice(2*i,2*i+2).join('');
+            if (arr[i] == '00') { arr[i] = ''};
+            if (arr[i] == '10') { arr[i] = '.'};
+            if (arr[i] == '11') { arr[i] = '-'};
+            if (arr[i] == '**') { arr[i] = '*'};
+    }
+    for (let i = 0; i < (arr.length/5); i++) {
+        arr2[i] = arr.slice(5*i,5*i+5).join('');
+        str = str + MORSE_TABLE[arr2[i]];
+    }
+    return str;
 }
-
 module.exports = {
     decode
 }
